4611a158209dc275b1f1f408ceb3660bf072dcb9
Jeong <inyeop0212@gmail.com>
2020-03-24 07:14:05 +0900

    Add login handler

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const randomstring = require('randomstring');
 
-app.get('/', function (req, res) {
-  res.send('index');
-});
+const queue = [];
+const rooms = {};
+const allUsers = {};
+const allNames = {};
 
 io.on('connection', function (socket) {
-  socket.on('login', function (data) {
-    console.log(data);
-  });
+  socket.on('login', name => {
+    const userId = socket.id;
 
-  socket.on('send message', function(data) {
-    console.log(data);
+    allUsers[userId] = socket;
+    allNames[userId] = name;
+
+    if (queue.length) {
+      const peerId = queue.pop();
+      const peer = allUsers[peerId];
+      const roomId = randomstring.generate();
+
+      peer.join(roomId);
+      socket.join(roomId);
+      rooms[peerId] = roomId;
+      rooms[userId] = roomId;
+      peer.emit('chat start', { name: allNames[userId], roomId });
+      socket.emit('chat start', { name: allNames[peerId], roomId } );
+    } else {
+      queue.push(userId);
+    }
   });
 });
 
